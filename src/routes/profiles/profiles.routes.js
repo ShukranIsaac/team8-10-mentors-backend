7a4ce12a3191333c45a4e702f7a8c438225ef50f
ShukranIsaac <imwakabira@ymail.com>
2022-11-17 09:43:53 +0200

    code refactor - profile routes

--- src/routes/profiles/profiles.routes.js
+++ src/routes/profiles/profiles.routes.js
@@ -8,25 +8,25 @@ const sendResponse = require('../../helpers/sendResponse')
 router.post('/profiles', async (req, res, next) => {
     const request = adaptRequest(req)
 
-    profileController.addOneProfile({ data: request.body }).then((result) => {
-        sendResponse(res, result)
-    }).catch((e) => sendResponse(res, e))
+    profileController.addOneProfile({ data: request.body })
+        .then((result) => sendResponse(res, result))
+        .catch((e) => sendResponse(res, e))
 })
 
 router.get('/profiles', async (req, res, next) => {
     const request = adaptRequest(req)
 
-    profileController.findAllProfiles({ data: request.body }).then((result) => {
-        sendResponse(res, result)
-    }).catch((e) => sendResponse(res, e))
+    profileController.findAllProfiles({ data: request.body })
+        .then((result) => sendResponse(res, result))
+        .catch((e) => sendResponse(res, e))
 })
 
 router.get('/profiles/:id', async (req, res, next) => {
     const request = adaptRequest(req)
 
-    profileController.findProfileById(request.pathParams.id).then((result) => {
-        sendResponse(res, result)
-    }).catch((e) => sendResponse(res, e))
+    profileController.findProfileById(request.pathParams.id)
+        .then((result) => sendResponse(res, result))
+        .catch((e) => sendResponse(res, e))
 })
 
 module.exports = router
