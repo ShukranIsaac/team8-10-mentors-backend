9ef2d62bb5abf16f4fa29eebaa0eaa6f9c552621
ShukranIsaac <imwakabira@ymail.com>
2022-11-18 13:21:13 +0200

    feat: list single role or all endpoint

--- src/routes/profiles/roles.routes.js
+++ src/routes/profiles/roles.routes.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const profileController = require('../../usecases/profiles/index')
+const profileRoleController = require('../../usecases/roles/index')
 const adaptRequest = require('../../helpers/adaptRequest')
 const sendResponse = require('../../helpers/sendResponse')
 
 router.get('/roles', async (req, res, next) => {
     const request = adaptRequest(req)
 
-    profileController.findAllProfiles({ data: request.body })
+    profileRoleController.findAllRoles({ data: request.body })
         .then((result) => sendResponse(res, result))
         .catch((e) => sendResponse(res, e))
 })
@@ -16,7 +16,7 @@ router.get('/roles', async (req, res, next) => {
 router.get('/roles/:id', async (req, res, next) => {
     const request = adaptRequest(req)
 
-    profileController.findProfileById(request.pathParams.id)
+    profileRoleController.findRoleById(request.pathParams.id)
         .then((result) => sendResponse(res, result))
         .catch((e) => sendResponse(res, e))
 })
